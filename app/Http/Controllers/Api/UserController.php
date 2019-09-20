bfeae35333949f9b9d0e15832ee400db3994e827
MH-Zarkhani <m.hassan.zarkhani@gmail.com>
2019-09-20 18:55:44 +0430

    create api for product

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::first();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::group(['namespace' => 'Api'], function() {
+Route::group(['namespace' => 'Api'], function (){
     Route::get('product','ProductController@index');
+
+    Route::group(['middleware' => 'auth:api'] , function (){
+        Route::get('user','UserController@index');
+    });
 });
